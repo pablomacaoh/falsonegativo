206342628c0a17c6c56632e62701fa6747cb0d1f
pablomacaoh <pabcastro@gmail.com>
2025-01-30 14:51:08 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
